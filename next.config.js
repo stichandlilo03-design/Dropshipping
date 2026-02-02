86c81ed404c3af96d0357d97638660f2fe811f2f
stichandlilo03-design <stichandlilo03@gmail.com>
2026-02-02 03:52:05 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // React strict mode for development
-  reactStrictMode: true,
-  
-  // Image optimization
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-  },
-  
-  // Handle dynamic pages that shouldn't be statically generated
   experimental: {
-    // This helps with dynamic routes
-    isrMemoryCacheSize: 52 * 50, // 52MB
+    cacheMaxMemorySize: 52 * 1024 * 1024, // 52MB (default is 50MB)
   },
 };
 
