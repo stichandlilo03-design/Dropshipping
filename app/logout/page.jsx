2aeaa9c50b89ec49b9d6b19acc060e33cb741c53
stichandlilo03-design <stichandlilo03@gmail.com>
2026-02-04 02:00:24 -0600

    Update page.jsx

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import PinterestPixel from './components/PinterestPixel';
 
 export const metadata = {
   title: 'DropBoard Dashboard',
@@ -9,37 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <noscript>
-          <img 
-            height="1" 
-            width="1" 
-            style={{display: 'none'}} 
-            alt="" 
-            src="https://ct.pinterest.com/v3/?event=init&tid=2612779406065&noscript=1" 
-          />
-        </noscript>
       </head>
       <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.pintrk = window.pintrk || function() {
-                (window.pintrk.queue = window.pintrk.queue || []).push(arguments);
-              };
-              window.pintrk.queue = window.pintrk.queue || [];
-              window.pintrk.version = "3.0";
-              
-              var n = document.createElement("script");
-              n.async = true;
-              n.src = "https://s.pinimg.com/ct/core.js";
-              var t = document.getElementsByTagName("script")[0];
-              t.parentNode.insertBefore(n, t);
-              
-              window.pintrk('load', '2612779406065');
-              window.pintrk('page');
-            `
-          }}
-        />
+        <PinterestPixel />
         {children}
       </body>
     </html>
