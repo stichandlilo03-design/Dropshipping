b03f72ee31e8d66a4734d1aa784c161d65d29ea7
stichandlilo03-design <stichandlilo03@gmail.com>
2026-02-04 02:04:44 -0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
 import './globals.css';
+import PinterestPixel from './components/PinterestPixel';
 
 export const metadata = {
   title: 'DropBoard Dashboard',
@@ -9,48 +7,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // 📌 PINTEREST TAG INITIALIZATION
-  useEffect(() => {
-    // Initialize Pinterest Tag
-    if (typeof window !== 'undefined' && !window.pintrk) {
-      window.pintrk = function () {
-        window.pintrk.queue.push(Array.prototype.slice.call(arguments));
-      };
-      const n = window.pintrk;
-      n.queue = [];
-      n.version = "3.0";
-      
-      const t = document.createElement("script");
-      t.async = true;
-      t.src = "https://s.pinimg.com/ct/core.js";
-      const r = document.getElementsByTagName("script")[0];
-      r.parentNode.insertBefore(t, r);
-    }
-
-    // Load Pinterest with your Pixel ID
-    if (typeof window !== 'undefined' && window.pintrk) {
-      window.pintrk('load', '2612779406065', {
-        em: '' // Will be set dynamically when user email is known
-      });
-      window.pintrk('page');
-    }
-  }, []);
-
   return (
     <html lang="en">
       <head>
-        {/* Pinterest noscript fallback */}
-        <noscript>
-          <img 
-            height="1" 
-            width="1" 
-            style={{display: 'none'}} 
-            alt="" 
-            src="https://ct.pinterest.com/v3/?event=init&tid=2612779406065&noscript=1" 
-          />
-        </noscript>
       </head>
-      <body>{children}</body>
+      <body>
+        <PinterestPixel />
+        {children}
+      </body>
     </html>
   );
 }
